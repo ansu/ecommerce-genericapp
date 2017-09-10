8990860e749797a75412d878403ed69e4e77404e
ansu jain <ansu.jain@kuliza.com>
2017-09-10 19:27:06 +0530

    init commit

---
+++ ecommerce-generic-app/Network.swift
@@ -0,0 +1,47 @@
+//
+//  Network.swift
+//  ecommerce-generic-app
+//
+//  Created by Kuliza-282 on 10/09/17.
+//  Copyright © 2017 Ansu. All rights reserved.
+//
+
+import Foundation
+
+
+enum NetworkAPIError: Error, CustomStringConvertible {
+    case requestFailed
+    case jsonConversionFailure
+    case invalidData
+    case responseUnsuccessful
+    case invalidURL
+    case jsonParsingFailure
+    
+    var description: String {
+        switch self {
+        case .invalidData: return "Received an invalid response"
+        case .invalidURL: return "URL is not valid"
+        default: return "Unkown error occurred"
+        }
+    }
+}
+
+
+protocol Network {
+    typealias JSON = [String: AnyObject]
+    func getAPIData(with request: URLRequest, completionHandler completion:@escaping (Result<JSON>) -> ())
+}
+
+enum Result <T>{
+    case Success(T)
+    case Error(ItunesApiError)
+}
+
+enum ItunesApiError: Error {
+    case requestFailed
+    case jsonConversionFailure
+    case invalidData
+    case responseUnsuccessful
+    case invalidURL
+    case jsonParsingFailure
+}
---
+++ ecommerce-generic-app/NetworkProvider.swift
@@ -0,0 +1,63 @@
+//
+//  NetworkProvider.swift
+//  ecommerce-generic-app
+//
+//  Created by Kuliza-282 on 10/09/17.
+//  Copyright © 2017 Ansu. All rights reserved.
+//
+
+import Foundation
+
+
+class NetworkProvider: Network {
+    
+    //1 creating the session
+    let session: URLSession
+    
+    //MARK: - Lifecycle
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+        
+    }
+    
+    typealias JSON = [String: AnyObject]
+    typealias JSONTaskCompletionHandler = (Result<JSON>) -> ()
+   
+    
+    // MARK: service methods
+    
+    func getAPIData(with request: URLRequest, completionHandler completion:@escaping JSONTaskCompletionHandler) {
+        
+        let task = self.session.dataTask(with: request as URLRequest) { (data, response, error) in
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.Error(.requestFailed))
+                return
+            }
+            
+            if httpResponse.statusCode == 200 {
+                if let data = data {
+                    do {
+                        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] {
+                            DispatchQueue.main.async {
+                                completion(.Success(json))
+                            }
+                        }
+                    } catch {
+                        completion(.Error(.jsonConversionFailure))
+                    }
+                } else {
+                    completion(.Error(.invalidData))
+                }
+            } else {
+                completion(.Error(.responseUnsuccessful))
+                print("\(error)")
+            }
+
+        }
+        task.resume()
+        
+    }
+    
+    
+}
